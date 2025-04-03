ad1a0d816778882a704015bc947ae1f74ae650a3
Nathália <nathanastacio@gmail.com>
2025-04-02 21:49:30 -0300

    Fixing PYTHON/RockPaperScissor.py errors

--- PYTHON/RockPaperScissor.py
+++ PYTHON/RockPaperScissor.py
@@ -1,53 +1,39 @@
+#Rock Paper Scissors Game in Python
+
 import random
-
 import os
-
 import re
 
+#Clean the console screen based on the operating system
 os.system('cls' if os.name=='nt' else 'clear')
 
+#Main game loop - runs indefinitely until manually terminated
 while (1 < 2):
+    print ("\n")
+    print ("Rock, Paper, Scissors - Shoot!")
 
-print ("\n")
+    #Get the user's choice, store it, and validate it using regular expression
+    userChoice = input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
+    if not re.match("[SsRrPp]", userChoice):
+        print ("Please choose a letter:")
+        print ("[R]ock, [S]cissors or [P]aper.")
 
-print ("Rock, Paper, Scissors - Shoot!")
+    # Display the user's choice
+    print ("You chose: " + userChoice)
 
-userChoice = input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
+    #Randomly select the computer's choice and displays it
+    choices = ['R', 'P', 'S']
+    opponenetChoice = random.choice(choices)
+    print ("I chose: " + opponenetChoice)
 
-if not re.match("[SsRrPp]", userChoice):
-
-print ("Please choose a letter:")
-
-print ("[R]ock, [S]cissors or [P]aper.")
-
-# Echo the user's choice
-
-print ("You chose: " + userChoice)
-
-choices = ['R', 'P', 'S']
-
-opponenetChoice = random.choice(choice)
-
-print ("I chose: " + opponenetChoice)
-
-if opponenetChoice == str.upper(userChoice) and str.upper(userChoice) == "P"
-
-print ("Tie! ")
-
-#if opponenetChoice == str("R") and str.upper(userChoice) == "P"
-
-elif opponenetChoice == 'R' and userChoice.upper() == 'S':
-
-print ("Rock beat scissors, I win! ")
-
-elif opponenetChoice == 'S' and userChoice.upper() == 'P':
-
-print ("scissors beat paper, I win! ")
-
-elif opponenetChoice == 'P' and userChoice.upper() == 'R':
-
-print ("Paper beat rock, I win!")
-
-else:
-
-print ("You win!")
+    #Determine the winner based on the games rules
+    if opponenetChoice == str.upper(userChoice) and str.upper(userChoice) == "P":
+        print ("Tie! ")
+    elif opponenetChoice == 'R' and userChoice.upper() == 'S':
+        print ("Rock beat scissors, I win! ")
+    elif opponenetChoice == 'S' and userChoice.upper() == 'P':
+        print ("scissors beat paper, I win! ")
+    elif opponenetChoice == 'P' and userChoice.upper() == 'R':
+        print ("Paper beat rock, I win!")
+    else:
+        print ("You win!")
